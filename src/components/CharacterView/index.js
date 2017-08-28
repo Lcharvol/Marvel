3b5d29233655df3927c17a2955ede90dabc10026
lcharvol <lucascharvolin@MacBook-Pro.local>
2017-08-28 23:19:14 +0200

    add close button

--- src/components/CharacterView/index.js
+++ src/components/CharacterView/index.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 import './CharacterView.css';
 
 const CharacterView = ({ toggleCharacter }) => (
-    <div className="characterview_container" onClick={() => toggleCharacter()}>
+    <div className="characterview_container">
         <div className="characterview_inner">
+            <i className="fa fa-times close_button" aria-hidden="true" onClick={() => toggleCharacter()}></i>
         </div>
     </div>
 );
